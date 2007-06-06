63bd16b3046d23ac3a1125f4ef24435c9fcf3851
Benilton Carvalho <bcarvalh@jhsph.edu>
2007-06-06 19:32:15 +0000

    adding the structure
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/maqcExpression4plex@338 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+.First.lib <- function(libname, pkgname) {
+  message(paste("/-------------------------------------------\\",
+                "| SAMPLE EXPRESSION DATA - MAQC/ HG18 - NGS |",
+                "|-------------------------------------------|",
+                "| Data provided by NimbleGen Systems (NGS). |",
+                "| This package is meant to be used only for |",
+                "| demonstration of BioConductor packages.   |",
+                "| Access http://www.hapmap.org for details. |",
+                "|-------------------------------------------|",
+                "| The contents of this package are provided |",
+                "| in good faith and the maintainer does not |",
+                "| warrant their accuracy.                   |",
+                "\\-------------------------------------------/",
+                sep="\n"))
+}
